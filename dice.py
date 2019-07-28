e2a9eacd44d9ec2a6ae897dcbaf573ac47aefff6
Nick Besse <nbesse@mail.sfsu.edu>
2019-07-28 01:07:59 -0700

    generalizing coin and dice

--- dice.py
+++ dice.py
@@ -1,6 +1,9 @@
 import random
+
+sides = 6
+
 class Dice():
 	#only ever need a random value from a Dice
 	def roll(self):
-		roll = random.randint(1,6)
+		roll = random.randint(1,sides)
 		return roll
